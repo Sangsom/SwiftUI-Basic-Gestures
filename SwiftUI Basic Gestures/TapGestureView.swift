e9ac5f33175f3aa125145f0836120821803476bb
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-05-13 12:17:21 +0300

    Created empty project and renamed ContentView to TapGestureView

--- SwiftUI Basic Gestures/TapGestureView.swift
+++ SwiftUI Basic Gestures/TapGestureView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TapGestureView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct TapGestureView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TapGestureView()
     }
 }

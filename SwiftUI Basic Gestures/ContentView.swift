74ca17b22b5e23f2283d96e56b058aeb9a8416ee
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-05-13 12:15:18 +0300

    Initial Commit

---
+++ SwiftUI Basic Gestures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Basic Gestures
+//
+//  Created by Rinalds Domanovs on 13/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Basic Gestures/SwiftUI_Basic_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Basic_GesturesApp.swift
+//  SwiftUI Basic Gestures
+//
+//  Created by Rinalds Domanovs on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Basic_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

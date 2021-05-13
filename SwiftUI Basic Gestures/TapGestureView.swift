93f380ae14c68589fcc80695e923cd8646e9f7f1
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-05-13 15:00:17 +0300

    Created 1x tap button

--- SwiftUI Basic Gestures/TapGestureView.swift
+++ SwiftUI Basic Gestures/TapGestureView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct TapGestureView: View {
+    @State private var isAnimating = false
+    @State private var tapped1x = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Tapped 1X: \(tapped1x) times")
+                .font(.caption)
+
+            Circle()
+                .frame(width: 80, height: 80)
+                .foregroundColor(.orange)
+                .overlay(
+                    Text("1X")
+                        .fontWeight(.medium)
+                )
+                .background(
+                    Circle()
+                        .strokeBorder(Color.blue, lineWidth: 3)
+                        .scaleEffect(isAnimating ? 1.5 : 1)
+                        .opacity(isAnimating ? 0 : 1)
+                )
+                .onTapGesture {
+                    tapped1x += 1
+                    withAnimation(Animation.easeOut(duration: 0.5)) {
+                        self.isAnimating = true
+                    }
+
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        self.isAnimating = false
+                    }
+            }
+        }
     }
 }
 
